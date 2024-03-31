9c6f1b68a39f0c76281aa44d7acc8af7b55251b7
SteveLorde <mostafa.maher98@gmail.com>
2024-03-31 16:07:06 +0200

    project init, created package.json, installed tailwind and postcss, removed bootstrap, created tailwind's minified app.mini.css, cleaned out NavMenu.razor, cleaned out MainLayout.razor

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{js,ts,jsx,tsx,cshtml,razor}",
+    "./NewsWebsiteServerApp/**/*.{js,ts,jsx,tsx,cshtml,razor}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
